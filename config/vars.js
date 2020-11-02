b4b1a7c4bd92ee11331390ccbac5ebb4a1a17467
Yael <eyaelabdissa8@gmail.com>
2020-11-02 10:53:44 +0300

    vars edited

--- config/vars.js
+++ config/vars.js
@@ -6,7 +6,7 @@ module.exports = {
     env: "development",
     port: 1000,
     mongo: {
-      uri: "mongodb://mongo:27017/FixMyCity",
+      uri: "mongodb://mongo:27017/FixMyCityT",
     },
     logs: 'production' ? 'combined' : 'dev',
     jwt_key: 'sampleRandomKey',
