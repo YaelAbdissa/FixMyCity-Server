278685b7b0c51813f6e4cfba543e052f31f9eb33
Yael <eyaelabdissa8@gmail.com>
2020-11-06 16:18:40 +0300

    update app.js

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ expressSwagger(options)
 mongoose.connect();
 
 app.use(logger('dev'));
-app.use(bodyparser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static('public/'));
 app.use(cors())
